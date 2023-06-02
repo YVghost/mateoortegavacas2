37ad112b9794141af1c38b8fd6f142eb4ee99b3d
Vghost <dddmateo@gmail.com>
2023-06-01 21:02:06 -0500

    fgdhj

--- funciones.c
+++ funciones.c
@@ -14,14 +14,15 @@ float largo(float x1, float y1, float z1, float x2, float y2, float z2){
     return dist;
 }
 
-void areasu (float base,float altura){
-    float hipotenusa = sqrt((pow(base,2)+(pow(altura,2))));
+void areasu (float base,float altura, float x1, float y1, float z1, float x2, float y2, float z2){
+    float hipotenusa = largo(x1, y1, z1, x2, y2, z2);
     float area= (pi*base*hipotenusa); 
-    printf("El area superficial es %f\n",area);
+    float areab=(pi*pow(base,2));
+    printf("El area superficial es %f\n",area + areab);
 }
 
 void volumen(float base,float altura){
-    float volumen = ((pi)*(pow(base,2)*altura))/3;
+    float volumen = ((pi)*(pow(base,2))*altura)/3;
     printf("El volumen es %f\n",volumen);
     
 }
